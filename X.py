86c52869144f2b4e1b04404174b24160af732142
Xavier Calle <49683608+XavierCalle@users.noreply.github.com>
2019-05-10 08:59:09 -0500

    Add files via upload

---
+++ X.py
@@ -0,0 +1,15 @@
+import turtle
+t=turtle.Pen()
+
+t.right(45)
+t.forward(200)
+
+t.penup()
+t.right(135)
+t.forward(141.42)
+
+t.pendown()
+t.right(135)
+t.forward(200)
+
+turtle.getscreen()._root.mainloop()
